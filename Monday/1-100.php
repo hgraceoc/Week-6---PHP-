df5a619cf809d38335b5febd03c8a1d6baccfd79
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-04 12:34:51 +0100

    print numbers 1-100 using a for loop

---
+++ Monday/1-100.php
@@ -0,0 +1,16 @@
+<?php
+
+// Write some code that lists the numbers from 1 to 100 using a for loop.
+
+// Example output:
+
+// int(1)
+// int(2)
+// int(3)
+// int(4)
+// …
+// int(100)
+
+for ($i = 1; $i <= 100; $i += 1) {
+    var_dump($i);
+}
