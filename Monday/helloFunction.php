ad8bd96ada2280e173e45103d99a8c7d7b4bc5b8
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-04 16:18:32 +0100

    function to add hello prior to a name

---
+++ Monday/helloFunction.php
@@ -0,0 +1,13 @@
+<?php
+
+// Update hello so that it returns the string "hello name". Also add parameter and return types.
+
+declare(strict_types=1);
+
+// add parameter and return types
+function hello(string $name) {
+    return "Hello " . $name;
+};
+
+var_dump(hello("alice")); // string(11) "hello alice"
+var_dump(hello("bob")); // string(9) "hello bob"
