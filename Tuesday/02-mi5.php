5b4c46574a24d2c303a5315aa78c3554570bc953
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-05 12:19:48 +0100

    00 followed by a digit

---
+++ Tuesday/02-mi5.php
@@ -0,0 +1,15 @@
+<?php
+
+// Create a function, mi5, that returns true if the given string is 00 followed by a digit. Make sure it returns a boolean value:
+
+
+function mi5(string $str) : bool {
+    return preg_match("/^(00)\d/", $str) ===1;
+} 
+
+// … your function here
+
+var_dump(mi5("007")); // bool(true)
+var_dump(mi5("oo8")); // bool(false)
+var_dump(mi5("003")); // bool(true)
+var_dump(mi5("M")); // bool(false)
