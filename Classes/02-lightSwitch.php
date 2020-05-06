7d312fdd100e945b0e4feafa1c03d01b4b3d486d
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-06 10:38:31 +0100

    updated light switch challenge with notes on methodology

--- Classes/02-lightSwitch.php
+++ Classes/02-lightSwitch.php
@@ -8,21 +8,25 @@ declare(strict_types=1);
 class LightSwitch
 {
     private $on = false;
+    //setting 'on'as an initial state, so that the isOn and turnOn and turnOff functions have a value to respond to 
 
     public function isOn()
     {
         return $this->on;
     }
+    //return a piece of information that already exists, i.e on = false (in its initial state)
 
     public function turnOn() 
     {
         $this->on = true;
     }
+    //$this-> refers to an instance of the class, it is a function which takes the value of false, and makes it true, so that the light is now on
 
     public function turnOff() 
     {
         $this->on = false;
     }
+    //to turn the lightswitch off the value now becomes false 
 
 }
 
